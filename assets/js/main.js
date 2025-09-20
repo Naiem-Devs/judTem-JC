28656931225d15cfbaa788af7ef7366de63c4430
Naiem-Devs <dnsnaiem@gmail.com>
2025-09-20 18:21:39 +0600

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,11 +65,16 @@ var swiper = new Swiper(".pdNavSwiper", {
 
 var swiper = new Swiper(".pdItemSwiper", {
 	effect: "slide",
-	slidesPerView: 3,
+	slidesPerView: 2,
 	spaceBetween: 15,
 	loop: true,
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
+	},
+	breakpoints: {
+		450: {
+			slidesPerView: 3,
+		},
 	},
 });
